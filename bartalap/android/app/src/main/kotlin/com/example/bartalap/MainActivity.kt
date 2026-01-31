09c8554d16399cd937e85b029e0a0437f6f76952
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2026-01-31 20:14:01 +0545

    refined: project structure and Flutter app setup

---
+++ bartalap/android/app/src/main/kotlin/com/example/bartalap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bartalap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ bartalap/lib/app_view.dart
@@ -0,0 +1,24 @@
+
+import 'package:bartalap/screens/home_screen.dart';
+import 'package:bartalap/theme/my_app_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "A R Y A",
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: MyAppTheme.blackColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: MyAppTheme.blackColor,
+          elevation: 0,
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
